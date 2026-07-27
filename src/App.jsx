64a8134657ace9567a6824520f69b0d2f7957318
thomas <thomaslazzeri3@gmail.com>
2026-07-27 20:32:40 +0200

    Add axios for API calls and implement actress data fetching in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [actresses, setActresess] = useState([]);
+
+  const API_URL = 'https://lanciweb.github.io/demo/api/actresses/'
+
+
+
+  useEffect(() => {
+    axios.get(API_URL)
+      .then(res => {
+        console.log(res);
+        setActresess(res.data)
+      });
+  }, []);
 
   return (
     <>
-      
+      <main>
+        {
+          actresses.map(item => (
+
+            <div key={item.id}>
+              <div>
+                {item.name}
+
+              </div>
+              <img src={item.image} alt="" />
+            </div>
+          ))
+        }
+      </main>
     </>
   )
 }
